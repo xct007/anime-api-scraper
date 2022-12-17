efd868405c05e0fd3b4b6efcbf05119010035770
xct007 <hackerxct007@gmail.com>
2022-12-17 20:14:37 +0700

    Finish

--- src/index.ts
+++ src/index.ts
@@ -1,68 +1,129 @@
-import axios from 'axios';
-import { API_LATEST, API_SEARCH, FetchData } from './utils';
+import axios from "axios";
+import { compareTwoStrings } from "string-similarity";
 
-async function latest(page: number, limit: number,): Promise<object|string|boolean> {
-    const URL = await API_LATEST(page, limit);
-    if (URL) {
-        let count: number = 0;
-        let result: any
-        while (true) {
-            const _data = await FetchData(`${URL}`);
-            if (_data == "Oops, API Key is Incorrect!" || !_data.status) {
-                if (count == 5) {
-                    result = false
-                    break
-                }
-                count = count + 1
-            } else if (_data.status) {
-                result = {
-                    retry_count: count,
-                    ..._data
-                }
-                break
-            }
-        }
-        return result
-    } else {
-        return false
-    }
-}
-async function search(query: string): Promise<object|boolean> {
-    const URL = await API_SEARCH();
-    if (URL) {
-        let count: number = 0;
-        let result: any
-        while (true) {
-            const _data = await FetchData(`${URL}`);
-            if (_data == "Oops, API Key is Incorrect!" || !_data.status) {
-                if (count == 5) {
-                    result = false
-                    break
-                }
-                count = count + 1
-            } else if (_data.status) {
+import {
+	API_LATEST,
+	API_SEARCH,
+	API_VIDEOS,
+	API_POST,
+	FetchData,
+} from "./utils";
 
-                // TODO: Cari category_name yang mirip-mirip di Array categories
-                // TODO: Find similar category_name in Array categories
-                try {
-                    const arrData = _data.categories.filter((val: any) => {
-                        return val.category_name === query // return object kosong
-                    })
-                    result = {
-                        retry_count: count,
-                        ...arrData
-                    }
-                } catch (error: any) {
-                    console.log(error)
-                    result = false
-                }
-                //
-                break
-            }
-        }
-        return result
-    } else {
-        return false
-    }
+async function latest(page: number, limit: number): Promise<object | boolean> {
+	const URL = await API_LATEST(page, limit);
+	if (URL) {
+		let count: number = 0;
+		let result: any;
+		while (true) {
+			const _data = await FetchData(`${URL}`);
+			if (_data == "Oops, API Key is Incorrect!" || !_data.status) {
+				if (count == 5) {
+					result = false;
+					break;
+				}
+				count = count + 1;
+			} else if (_data.status) {
+				result = {
+					retry_count: count,
+					..._data,
+				};
+				break;
+			}
+		}
+		return result;
+	} else {
+		return false;
+	}
 }
-search("naruto").then((a: object|boolean) => console.log(a))
+type searchResponse = {
+	status: boolean;
+	retry_count: number;
+	result?: any[] | string;
+};
+async function search(query: string): Promise<searchResponse | boolean> {
+	const URL = await API_SEARCH();
+	if (URL) {
+		let count: number = 0;
+		let result: any;
+		while (true) {
+			const _data = await FetchData(`${URL}`);
+			if (_data == "Oops, API Key is Incorrect!" || !_data.status) {
+				if (count == 5) {
+					result = false;
+					break;
+				}
+				count = count + 1;
+			} else if (_data.status == "ok") {
+				const arrData = _data.categories.filter((val: any) => {
+					return compareTwoStrings(String(val.category_name), query) > 0.3;
+				});
+				if (arrData) {
+					result = {
+						status: true,
+						retry_count: count,
+						result: arrData,
+					};
+				} else {
+					result = {
+						status: false,
+						retry_count: count,
+						result: "Not Found",
+					};
+				}
+				break;
+			}
+		}
+		return result;
+	} else {
+		return false;
+	}
+}
+async function detail(id: number): Promise<object | boolean> {
+	const URL = await API_VIDEOS(id);
+	if (URL) {
+		let count: number = 0;
+		let result: any;
+		while (true) {
+			const _data = await FetchData(`${URL}`);
+			if (_data == "Oops, API Key is Incorrect!" || !_data.status) {
+				if (count == 5) {
+					result = false;
+					break;
+				}
+				count = count + 1;
+			} else if (_data.status) {
+				result = {
+					retry_count: count,
+					..._data,
+				};
+				break;
+			}
+		}
+		if (!result.posts.length) {
+			count = 0;
+			const _URL = await API_POST(id);
+			while (true) {
+				const _data_ = await FetchData(`${_URL}`);
+				if (_data_ == "Oops, API Key is Incorrect!" || !_data_.status) {
+					if (count == 5) {
+						result = false;
+						break;
+					}
+					count = count + 1;
+				} else if (_data_.status) {
+					result = {
+						retry_count: count,
+						..._data_,
+					};
+					break;
+				}
+			}
+		} else {
+			result = false;
+		}
+		return result;
+	} else {
+		return false;
+	}
+}
+export { latest, search, detail };
--- src/utils.ts
+++ src/utils.ts
@@ -1,63 +1,85 @@
-import axios from 'axios';
-const API_URL = 'https://pencarinafkah.xyz/com.animeku.animechannelsubindoandsubenglish'
+import axios from "axios";
+const API_URL =
+	"https://pencarinafkah.xyz/com.animeku.animechannelsubindoandsubenglish";
 
-const API_LATEST = async(page: number, limit: number): Promise<string|boolean> => {
-    const Data = await keyApi();
-    if (Data) {
-        return `${Data.baseUrl}/api/get_videos?page=${page}&count=${limit}&api_key=${Data.apikey}`
-    } else {
-        return false
-    }
-}
-const API_SEARCH = async(): Promise<string|boolean> => {
-    const Data = await keyApi();
-    if (Data) {
-        return `${Data.baseUrl}/api/get_category_genre?search=&sort=c.category_name%20ASC&api_key=${Data.apikey}`
-    } else {
-        return false
-    }
-}
+const API_LATEST = async (
+	page: number,
+	limit: number
+): Promise<string | boolean> => {
+	const Data = await keyApi();
+	if (Data) {
+		return `${Data.baseUrl}/api/get_videos?page=${page}&count=${limit}&api_key=${Data.apikey}`;
+	} else {
+		return false;
+	}
+};
+const API_SEARCH = async (): Promise<string | boolean> => {
+	const Data = await keyApi();
+	if (Data) {
+		return `${Data.baseUrl}/api/get_category_genre?search=&sort=c.category_name%20ASC&api_key=${Data.apikey}`;
+	} else {
+		return false;
+	}
+};
+const API_POST = async (vid: number): Promise<string | boolean> => {
+	const Data = await keyApi();
+	if (Data) {
+		return `${Data.baseUrl}/api/get_post_detail?id=${vid}`;
+	} else {
+		return false;
+	}
+};
+const API_VIDEOS = async (id: number): Promise<string | boolean> => {
+	const Data = await keyApi();
+	if (Data) {
+		return `${Data.baseUrl}/api/get_category_videos?id=${id}&page=1&count=1200&sort=n.id%20DESC&api_key=${Data.apikey}`;
+	} else {
+		return false;
+	}
+};
 type RespOnKey = {
-    status: boolean,
-    baseUrl?: string,
-    api_yt?: string,
-    apikey?: string
-}
+	status: boolean;
+	baseUrl?: string;
+	api_yt?: string;
+	apikey?: string;
+};
 async function keyApi(): Promise<RespOnKey> {
-    const { data } = await axios.get(API_URL, {
-        headers: {
-            "User-Agent": "Dalvik/2.1.0 (Linux; U; Android 12; M2007J20CG Build/SKQ1.211019)"
-        }
-    });
-    if (data) {
-        const isOk = data?.Iklan[0]
-        if (isOk.status == 'ok') {
-            return {
-                status: true,
-                baseUrl: isOk.webbase,
-                api_yt: isOk.api_yt,
-                apikey: isOk.apikey
-            }
-        } else {
-            return {
-                status: false
-            }
-        }
-    } else {
-        return {
-            status: false
-        }
-    }
+	const { data } = await axios.get(API_URL, {
+		headers: {
+			"User-Agent":
+				"Dalvik/2.1.0 (Linux; U; Android 12; M2007J20CG Build/SKQ1.211019)",
+		},
+	});
+	if (data) {
+		const isOk = data?.Iklan[0];
+		if (isOk.status == "ok") {
+			return {
+				status: true,
+				baseUrl: isOk.webbase,
+				api_yt: isOk.api_yt,
+				apikey: isOk.apikey,
+			};
+		} else {
+			return {
+				status: false,
+			};
+		}
+	} else {
+		return {
+			status: false,
+		};
+	}
 }
-const FetchData = async(url: string): Promise<any> => {
-    const { data } = await axios.get(`${url}`, {
-        headers: {
-            //"Data-Agent": "Your Videos Channel",
-            "User-Agent": "Dalvik/7.1.12.1.0 (com.animeku.animechannelsubindoandsubenglish U; Android ; 20175 Build/NMF260)",
-            //"Accept": "application/vnd.yourapi.v1.full+json",
-            "Accept-Encoding": " "
-        }
-    })
-    return data
-}
-export { API_LATEST, API_SEARCH, FetchData }
+const FetchData = async (url: string): Promise<any> => {
+	const { data } = await axios.get(`${url}`, {
+		headers: {
+			//"Data-Agent": "Your Videos Channel",
+			"User-Agent":
+				"Dalvik/7.1.12.1.0 (com.animeku.animechannelsubindoandsubenglish U; Android ; 20175 Build/NMF260)",
+			//"Accept": "application/vnd.yourapi.v1.full+json",
+			"Accept-Encoding": " ",
+		},
+	});
+	return data;
+};
+export { API_LATEST, API_SEARCH, API_VIDEOS, API_POST, FetchData };
